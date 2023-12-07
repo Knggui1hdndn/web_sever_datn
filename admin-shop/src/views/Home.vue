71adbba3d7f2916e4f557104723553a81599e247
caube <thanhkaka22>
2023-12-07 13:59:57 +0700

    Updatecode

--- admin-shop/src/views/Home.vue
+++ admin-shop/src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <h3> Bảng điều khiển</h3>
   <div>
     <!-- Top Section -->
     <div class="top-section">
@@ -7,113 +8,112 @@
           <div class="col">
             <div class="card text-bg-primary">
               <div class="card-body">
-                <h5 class="card-title">Tiêu Đề 1</h5>
+                <h5 class="card-title">Số lượng sản phâm</h5>
                 <p class="card-text">{{ dataTop1 }}</p>
-                <a href="#" class="arrow-link">Điều hướng <i class="fas fa-arrow-right"></i></a>
+                <a href="#" class="arrow-link"><i class="fas fa-arrow-right"></i></a>
               </div>
             </div>
+            <div class="col">
+            <div class="card text-bg-success">
+              <div class="card-body">
+                <h5 class="card-title">Số Lượng sản phẩm đã bán</h5>
+                <p class="card-text">{{ dataTop3 }}</p>
+                <a href="#" class="arrow-link"> <i class="fas fa-arrow-right"></i></a>
+              </div>
+            </div>
+            
+          </div>
           </div>
           <div class="col">
             <div class="card text-bg-secondary">
               <div class="card-body">
-                <h5 class="card-title">Tiêu Đề 2</h5>
+                <h5 class="card-title">Số lượng khách hàng </h5>
                 <p class="card-text">{{ dataTop2 }}</p>
-                <a href="#" class="arrow-link">Điều hướng <i class="fas fa-arrow-right"></i></a>
+                <a href="#" class="arrow-link"> <i class="fas fa-arrow-right"></i></a>
               </div>
             </div>
-          </div>
-          <div class="col">
+            <div class="col">
             <div class="card text-bg-success">
               <div class="card-body">
                 <h5 class="card-title">Tiêu Đề 3</h5>
                 <p class="card-text">{{ dataTop3 }}</p>
-                <a href="#" class="arrow-link">Điều hướng <i class="fas fa-arrow-right"></i></a>
+                <a href="#" class="arrow-link"> <i class="fas fa-arrow-right"></i></a>
               </div>
             </div>
+          </div>
+          </div>
+          <div class="col">
+            <div class="card text-bg-success">
+              <div class="card-body">
+                <h5 class="card-title">Số lượng đơn hàng</h5>
+                <p class="card-text">{{ dataTop3 }}</p>
+                <a href="#" class="arrow-link"> <i class="fas fa-arrow-right"></i></a>
+              </div>
+            </div>
+            <div class="col">
+            <div class="card text-bg-success">
+              <div class="card-body">
+                <h5 class="card-title">Tổng doanh số </h5>
+                <p class="card-text">{{ dataTop3 }}</p>
+                <a href="#" class="arrow-link"> <i class="fas fa-arrow-right"></i></a>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
       </div>
     </div>
+      </div>
+      <div>
+      
+  <div class="model-body">
+  <form>
+    <h6>Ngày bắt đầu</h6>
+    <input type="datetime-local" class="from">
+  </form>
+  <form>
+    <h6>Ngày kết thúc</h6>
+    <input type="datetime-local" class="from">
+  </form>
+</div>
+      </div>
 
-    <!-- Bottom Section -->
-    <div class="bottom-section">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <div class="card text-bg-danger">
-              <div class="card-body">
-                <h5 class="card-title">Tiêu Đề 4</h5>
-                <p class="card-text">{{ dataBottom1 }}</p>
-                <a href="#" class="arrow-link">Điều hướng <i class="fas fa-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card text-bg-warning">
-              <div class="card-body">
-                <h5 class="card-title">Tiêu Đề 5</h5>
-                <p class="card-text">{{ dataBottom2 }}</p>
-                <a href="#" class="arrow-link">Điều hướng <i class="fas fa-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card text-bg-info">
-              <div class="card-body">
-                <h5 class="card-title">Tiêu Đề 6</h5>
-                <p class="card-text">{{ dataBottom3 }}</p>
-                <a href="#" class="arrow-link">Điều hướng <i class="fas fa-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-        
-      </div>
-    </div>
-    <div>
-      <b-form-group label="Chọn khoảng ngày">
-      <b-row>
-        <b-col>
-          <b-form-datepicker
-            v-model="startDate"
-            placeholder="Ngày bắt đầu"
-            class="mb-2"
-          ></b-form-datepicker>
-        </b-col>
-        <b-col>
-          <b-form-datepicker
-            v-model="endDate"
-            placeholder="Ngày kết thúc"
-          ></b-form-datepicker>
-        </b-col>
-      </b-row>
-    </b-form-group>
-    <b-form-group label="Chọn khoảng ngày">
-      <b-row>
-        <b-col>
-          <b-form-datepicker
-            v-model="startDate"
-            placeholder="Ngày bắt đầu"
-            class="mb-2"
-          ></b-form-datepicker>
-        </b-col>
-        <b-col>
-          <b-form-datepicker
-            v-model="endDate"
-            placeholder="Ngày kết thúc"
-          ></b-form-datepicker>
-        </b-col>
-      </b-row>
-    </b-form-group>
-  </div>
-  </div>
+
+
+    
+    <h3>Tốp 5 sản phẩm bán chạy nhất </h3>
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">STT</th>
+          <th scope="col">Tên sản phẩm</th>
+          <th scope="col">Ảnh </th>
+          <th scope="col">Giá</th>
+          <th scope="col">Số Lượng đã bán</th>
+          <th scope="col">Thể Loại</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="item.id">
+          <th scope="row">{{ ++index }}</th>
+          <td>{{ item.name }}</td>
+          <td>
+            <img :src="item.avatar" alt="Avatar" class="avatar-image" />
+          </td>
+          <td>{{ item.email }}</td>
+          <td>{{ item.phoneNumber }}</td>
+          <td>{{ item.address }}</td>
+          <td>
+            <select name="emp" :disabled="true">
+              <option>{{ item.employed ? 'Yes' : 'No' }}</option>
+            </select>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  
 </template>
-
 <script setup>
-
-
-
   const dataTop1 = "123";
   const dataTop2 = "456";
   const dataTop3 = "789";
@@ -123,7 +123,6 @@
 </script>
 
 <style scoped>
-  /* Add any custom styles as needed */
   .top-section,
   .bottom-section {
     padding: 50px 0;
@@ -166,4 +165,12 @@
   .text-bg-success {
     background-color: #28a745; /* Bootstrap success color */
   }
+  .model-body {
+  display: flex;
+}
+
+.model-body form {
+  margin-right: 20px; /* hoặc bất kỳ giá trị margin nào phù hợp */
+}
+
   </style>
--- admin-shop/src/views/product/ListProduct.vue
+++ admin-shop/src/views/product/ListProduct.vue
@@ -46,18 +46,10 @@
             <div class="modal fade" id="deleteModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Xóa sản phẩm</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
+                 
                   
-                  <div class="modal-body">
-                    Bạn có chắc muốn xóa sản phẩm <span class="fw-bold">{{ item.name }}</span>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-danger">Save changes</button>
-                  </div>
+                
+                
                 </div>
               </div>
             </div>
